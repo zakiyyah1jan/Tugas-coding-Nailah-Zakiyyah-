591deb75fc8d77b737fb0bd71b2a37f151130382
zakiyyah1jan <nailahzakiyyah165@gmail.com>
2025-10-01 22:31:50 +0700

    Add files via upload

---
+++ DATA STRING.py
@@ -0,0 +1,7 @@
+nama_depan = "nailah"
+nama_belakang = 'zakiyyah'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '12'
+alamat = 'air itam'
+kata_mutiara = "ikuti seperti air mengalir"
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
